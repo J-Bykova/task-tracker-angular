751eb3010ef65d12fe0e87e5016c54dc4b3852da
Jenny B <jenia.bykova@gmail.com>
2022-12-12 11:12:12 -0800

    Add a few components

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'December 16th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Angular class',
+    day: 'December 12th at 9:00am',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'David English class',
+    day: 'December 13th at 10:00am',
+    reminder: false,
+  },
+  {
+    id: 4,
+    text: 'Alex English class',
+    day: 'December 12th at 06:00pm',
+    reminder: false,
+  },
+];
